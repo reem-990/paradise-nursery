445b035fe67e76239f5df1d35951da94f80a5d26
reem-990 <reemmubarak43@gmail.com>
2026-01-20 02:24:36 +0300

    Add AboutUs component for Paradise Nursery
    Add About Us page

---
+++ aboutus.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h1>عن Paradise Nursery</h1>
+      <p>
+        Paradise Nursery 
+      </p>
+      <p>
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
